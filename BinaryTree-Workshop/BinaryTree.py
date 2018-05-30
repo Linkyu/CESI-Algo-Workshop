4055e445a173eed890cf6afe98b713d068ff08c9
Linkyu <linkyu.work@gmail.com>
2018-05-30 16:07:17 +0200

    Finish workshop

--- BinaryTree-Workshop/BinaryTree.py
+++ BinaryTree-Workshop/BinaryTree.py
@@ -4,6 +4,9 @@ class Node(object):
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return self.weight
+
     def insert(self, i):
         if i < self.weight:
             if self.left:
@@ -15,3 +18,25 @@ class Node(object):
                 self.right.insert(i)
             else:
                 self.right = Node(i, None, None)
+
+    def print_infix(self) -> [int]:
+        return (self.left.print_infix() if self.left else []) + [self.weight] + (self.right.print_infix() if self.right else [])
+
+    def print_prefix(self) -> [int]:
+        return [self.weight] + (self.left.print_prefix() if self.left else []) + (self.right.print_prefix() if self.right else [])
+
+    def print_postfix(self) -> [int]:
+        return (self.left.print_postfix() if self.left else []) + (self.right.print_postfix() if self.right else []) + [self.weight]
+
+
+# Main
+values = [21, 9, 12, 34, 36, 35, 5, 8, 1, 25]
+root = Node(values[0], None, None)
+
+for value in values[1:]:
+    root.insert(value)
+
+print("values:  " + str(values))
+print("infix:   " + str(root.print_infix()))
+print("prefix:  " + str(root.print_prefix()))
+print("postfix: " + str(root.print_postfix()))
